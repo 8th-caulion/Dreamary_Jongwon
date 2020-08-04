10f7c80874fe2ed04f37875a964690785040871d
JongwonYoun <jeffyoun@naver.com>
2020-08-04 21:59:41 +0900

    FEAT: introduce 페이지 추가

--- dreamaryproject/urls.py
+++ dreamaryproject/urls.py
@@ -19,5 +19,6 @@ from page import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name = "home")
+    path('', views.home, name = "home"),
+    path('introduce/', views.introduce, name="introduce"),
 ]
